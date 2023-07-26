fdffb366a65ac39e34959ef1bc2b17d9440b540f
Owen Rumney <owen.rumney@ghost.security>
2023-07-26 21:38:05 +0100

    feat: updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,23 @@ jQuery(document).ready(function($) {
 
         $('.level-bar-inner').each(function() {
         
-            var itemWidth = $(this).data('level');
+            var from = $(this).data('from');
+            var till = $(this).data('till');
+            var level = $(this).data('level');
             
+            // var parentwidth = this.parentNode.clientWidth;
+            // var year = parentwidth/18;
+            // var start = from - 2005;
+            // var end = 2023 - till;
+
+            // $(this).animate({
+            //     width: parentwidth - (end * year) - (start * year),
+            //     left: start * year
+            // }, 800);
+
             $(this).animate({
-                width: itemWidth
-            }, 800);
+                width: level
+            })
             
         });
 
